e5504543a007cf9a246a992280b4d9152b3a1282
SamitaTT <m.samita.tt@gmail.com>
2023-12-06 14:41:12 +0700

    feat(CB&HOF): completed ex1-2

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,20 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  if (operation(array) >= 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function pass70(scores) {
+  let passStudent = 0;
+  for (let i = 0; i < scores.length; i++)
+    if (scores[i] >= 70) {
+      passStudent++;
+    }
+  return passStudent;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +23,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, pass70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, pass70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, pass70);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
